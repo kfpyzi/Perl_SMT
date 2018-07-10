3e60cc8fc925978112d9d051df68e379944f50fb
Kyle Farinas <kyle.farinas@adish.co.jp>
2018-07-10 14:17:20 +0800

    SMT model module translation model added

--- SMT/models.pm
+++ SMT/models.pm
@@ -55,3 +55,112 @@ sub _language_model{
     print("\n");    
     
 }
+
+sub _translation_model{
+    print("translation model entered...\n");
+
+    my $TREN = "/Users/kylefarinas/Desktop/kyle/test/sys/small_parallel_enja-master/train.en";
+    my $TRJA = "/Users/kylefarinas/Desktop/kyle/test/sys/small_parallel_enja-master/train.ja";
+
+    open(my $bi, '>', 'BIENJA.txt');
+    
+    #open(my $bien, '<', $TREN) or die "data: $!\n";
+    #open(my $bija, '<', $TRJA) or die "data: $!\n";
+
+
+
+    open(my $bien, '<', $TREN) or die "data: $!\n"; 
+    open(my $bija, '<', $TRJA) or die "data: $!\n";
+
+    chomp(my @EN_TL = <$bien>);
+    chomp(my @JP_TL = <$bija>);
+
+    my @ENT_tokens = ();
+    foreach my $EN_T (@EN_TL){
+        my @EN_token = split(/ /, $EN_T);
+        push @EN_token, "</s>";
+        unshift @EN_token, "<s>";
+        push (@ENT_tokens, [@EN_token]);
+    }
+
+    my @JPT_tokens = ();
+    foreach my $JP_T (@JP_TL){
+        my @JP_token = split(/ /, $JP_T);
+        push @JP_token, "</s>";
+        unshift @JP_token, "<s>";
+        push (@JPT_tokens, [@JP_token]);
+    }
+
+    my %TJ;
+    my %CEJ;
+    my %TEF;
+    foreach my $x (0.. ($#ENT_tokens + $#JPT_tokens)/2){
+        my @arr = @{$ENT_tokens[$x]};
+        my @arr2 = @{$JPT_tokens[$x]};
+        #set count(e|f) to 0 for all e,f
+        for(my $i = 0; $i < $#arr; $i++){
+            
+            for (my $j = 0; $j < $#arr2; $j++){
+                $CEJ{$arr[$i]}{$arr2[$j]} = 0;
+                $TEF{$arr[$i]}{$arr2[$j]}++;
+                #$TJ{$arr2[$j]} = 0;
+                #print($CEJ{$arr[$i]}{$arr2[$j]});
+            }
+        }
+        #set total(f) to 0 for all f
+        for (my $a = 0; $a < $#arr2; $a++){
+            $TJ{$arr2[$a]} = 0;
+        }
+    }
+
+    my %TSE;
+    #for all sentence pairs
+    foreach my $y (0.. ($#ENT_tokens + $#JP_T)/2){
+        my @arrE = @{$ENT_tokens[$y]};
+        my @arrJ = @{$JPT_tokens[$y]};
+        #set total_se = 0
+        for(my $i = 0; $i < $#arrE; $i++){
+            $TSE{$arrE[$i]} = 0;
+        }
+=pod
+        for all words e in e_s
+            for all words f in f_s
+                total_s(e) += t(e|f)
+        for all words e in e_s
+            for all words f in f_s
+                count(e|f) += t(e|f) / total_s(e)
+                total(f)   += t(e|f) / total_s(e)
+=cut
+        for(my $se = 0; $se < $#arrE; $se++){
+            for(my $sj = 0; $sj < $#arrJ; $sj++){
+                $TSE{$arrE[$se]} += $TEF{$arrE[$se]}{$arrJ[$sj]};
+                $CEJ{$arrE[$se]}{$arrJ[$sj]} += $TEF{$arrE[$se]}{$arrJ[$sj]} / $TSE{$arrE[$se]};
+                $TJ{$arrJ[$sj]} += $TEF{$arrE[$se]}{$arrJ[$sj]} / $TSE{$arrE[$se]};
+            }
+        }
+
+
+    }
+
+    foreach my $z (0.. ($#ENT_tokens + $#JP_T)/2){
+        my @arrEF = @{$ENT_tokens[$z]};
+        my @arrJF = @{$JPT_tokens[$z]};
+        for(my $i = 0; $i < $#arrEF; $i++){
+            for (my $j = 0; $j < $#arrJF; $j++){
+                
+                $TEF{$arrEF[$i]}{$arrJF[$j]} = $CEJ{$arrEF[$i]}{$arrJF[$j]} / $TJ{$arrJF[$j]};
+            }
+        }
+    }
+
+    #print Dumper @ENT_tokens;
+
+    print("translation model created...\n");
+    #print Dumper @JPT_tokens;
+    return \%TEF;
+    print("\n");    
+    
+    
+}
+
+1;
